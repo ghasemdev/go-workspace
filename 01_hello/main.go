dcbf3d710758cd5fe779349ddff35bd94993652e
Ghasem Shirdel <shirdelghasem79@gmail.com>
2024-10-10 12:31:47 +0330

    add hello world

---
+++ 01_hello/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	StrUtils "go-workspace/packages"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println(StrUtils.Reverse("Hello"))
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"go-workspace/packages"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println(packages.Reverse("Hello"))
+}
